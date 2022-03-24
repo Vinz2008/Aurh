43e1024a3aa83c0b0ba6e381175c43eea30f1b07
Vinz2008 <vincentbidarddelanoe@gmail.com>
2022-03-24 20:29:45 +0100

    fixed folder where it cloned

--- aurh.sh
+++ aurh.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [[ $1 = "-S" ]]; then
-git clone https://aur.archlinux.org/$2.git
-cd $2
+git clone https://aur.archlinux.org/$2.git /tmp/$2
+cd /tmp/$2
 makepkg -si
-cd ..
-rm -rf  $2
+cd ~
+rm -rf  /tmp/$2
 else
 echo 'Use a valid option'
 fi
