d20da0a6d7175ca51dce3734cd8d1d3f927a984e
Vinz2008 <vincentbidarddelanoe@gmail.com>
2022-09-11 18:14:53 +0200

    fixed instruction that made installing aur package not work

--- main.c
+++ main.c
@@ -139,7 +139,6 @@ int testPacmanPackageExist(char repoName[20]){
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
         curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-        curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1);
         res = curl_easy_perform(curl);
         printf("TEST\n");
         if(res != CURLE_OK) {
